403382ae9a1a49f056c2b23d5d614c9f81a8cb5b
Ryan C. Gordon <icculus@icculus.org>
2022-03-03 00:28:41 -0500

    Video numero cuatro! I struggle to get audio streaming to work! But I do!!
    The accompanying video to follow along: https://youtu.be/7Q4VHSwPggs

--- sdlamp.c
+++ sdlamp.c
@@ -16,11 +16,45 @@ static void panic_and_abort(const char *title, const char *text)
     exit(1);
 }
 
+static Uint8 *wavbuf = NULL;
+static Uint32 wavlen = 0;
+static SDL_AudioSpec wavspec;
+static SDL_AudioStream *stream = NULL;
+
+static SDL_bool open_new_audio_file(const char *fname)
+{
+    SDL_FreeAudioStream(stream);
+    stream = NULL;
+    SDL_FreeWAV(wavbuf);
+    wavbuf = NULL;
+    wavlen = 0;
+
+    if (SDL_LoadWAV(fname, &wavspec, &wavbuf, &wavlen) == NULL) {
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Couldn't load wav file!", SDL_GetError(), window);
+        return SDL_FALSE;
+    }
+
+    stream = SDL_NewAudioStream(wavspec.format, wavspec.channels, wavspec.freq, AUDIO_F32, 2, 48000);
+    if (!stream) {
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Couldn't create audio stream!", SDL_GetError(), window);
+        SDL_FreeWAV(wavbuf);
+        wavbuf = NULL;
+        wavlen = 0;
+        return SDL_FALSE;
+    }
+
+    if (SDL_AudioStreamPut(stream, wavbuf, wavlen) == -1) {  // !!! FIXME: graceful handling.
+        panic_and_abort("Audio stream put failed", SDL_GetError());
+    }
+
+    SDL_AudioStreamFlush(stream);  // !!! FIXME: error handling
+
+    return SDL_TRUE;
+}
+
 int main(int argc, char **argv)
 {
-    Uint8 *wavbuf = NULL;
-    Uint32 wavlen = 0;
-    SDL_AudioSpec wavspec;
+    SDL_AudioSpec desired;
 
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) == -1) {
         panic_and_abort("SDL_Init failed", SDL_GetError());
@@ -36,7 +70,21 @@ int main(int argc, char **argv)
         panic_and_abort("SDL_CreateRenderer failed", SDL_GetError());
     }
 
+    SDL_zero(desired);
+    desired.freq = 48000;
+    desired.format = AUDIO_F32;
+    desired.channels = 2;
+    desired.samples = 4096;
+    desired.callback = NULL;
+
+    audio_device = SDL_OpenAudioDevice(NULL, 0, &desired, NULL, 0);
+    if (audio_device == 0) {
+        panic_and_abort("Couldn't audio device!", SDL_GetError());
+    }
+
     SDL_EventState(SDL_DROPFILE, SDL_ENABLE);  // tell SDL we want this event that is disabled by default.
+
+    open_new_audio_file("music.wav");
 
     SDL_bool paused = SDL_TRUE;
     const SDL_Rect rewind_rect = { 100, 100, 100, 100 };
@@ -54,59 +102,35 @@ int main(int argc, char **argv)
 
                 case SDL_MOUSEBUTTONDOWN: {
                     const SDL_Point pt = { e.button.x, e.button.y };
-                    if (SDL_PointInRect(&pt, &rewind_rect)) {
-                        if (audio_device) {
-                            SDL_ClearQueuedAudio(audio_device);
-                            SDL_QueueAudio(audio_device, wavbuf, wavlen);
+                    if (SDL_PointInRect(&pt, &rewind_rect)) {  // pressed the "rewind" button
+                        SDL_ClearQueuedAudio(audio_device);
+                        SDL_AudioStreamClear(stream);
+                        if (SDL_AudioStreamPut(stream, wavbuf, wavlen) == -1) {  // !!! FIXME: graceful handling.
+                            panic_and_abort("Audio stream put failed", SDL_GetError());
                         }
-                    } else if (SDL_PointInRect(&pt, &pause_rect)) {
+                        SDL_AudioStreamFlush(stream);  // !!! FIXME: error handling
+                    } else if (SDL_PointInRect(&pt, &pause_rect)) {  // pressed the "pause" button
                         paused = paused ? SDL_FALSE : SDL_TRUE;
-                        if (audio_device) {
-                            SDL_PauseAudioDevice(audio_device, paused);
-                        }
+                        SDL_PauseAudioDevice(audio_device, paused);
                     }
                     break;
                 }
 
                 case SDL_DROPFILE: {
-                    if (audio_device) {
-                        SDL_CloseAudioDevice(audio_device);
-                        audio_device = 0;
-                    }
-
-                    SDL_FreeWAV(wavbuf);
-                    wavbuf = NULL;
-                    wavlen = 0;
-
-                    if (SDL_LoadWAV(e.drop.file, &wavspec, &wavbuf, &wavlen) == NULL) {
-                        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Couldn't load wav file!", SDL_GetError(), window);
-                    }
-
-                    #if 0  // !!! FIXME: come back to this.
-                    SDL_AudioSpec desired;
-                    SDL_zero(desired);
-                    desired.freq = 48000;
-                    desired.format = AUDIO_F32;
-                    desired.channels = 2;
-                    desired.samples = 4096;
-                    desired.callback = NULL;
-                    #endif
-
-                    audio_device = SDL_OpenAudioDevice(NULL, 0, &wavspec, NULL, 0);
-                    if (audio_device == 0) {
-                        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Couldn't audio device!", SDL_GetError(), window);
-                        SDL_FreeWAV(wavbuf);
-                        wavbuf = NULL;
-                        wavlen = 0;
-                    } else {
-                        SDL_QueueAudio(audio_device, wavbuf, wavlen);
-                        SDL_PauseAudioDevice(audio_device, paused);
-                    }
-
+                    open_new_audio_file(e.drop.file);
                     SDL_free(e.drop.file);
                     break;
                 }
+            }
+        }
 
+        if (SDL_GetQueuedAudioSize(audio_device) < 8192) {
+            const int bytes_remaining = SDL_AudioStreamAvailable(stream);
+            if (bytes_remaining > 0) {
+                const int new_bytes = SDL_min(bytes_remaining, 32 * 1024);
+                static Uint8 converted_buffer[32 * 1024];
+                SDL_AudioStreamGet(stream, converted_buffer, new_bytes);  // !!! FIXME: error checking
+                SDL_QueueAudio(audio_device, converted_buffer, new_bytes);
             }
         }
 
