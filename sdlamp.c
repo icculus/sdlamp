27073c4e9e096f386ebbe966975656690dd6cef4
Ryan C. Gordon <icculus@icculus.org>
2022-03-12 01:12:22 -0500

    The ninth video! The Great Refactor!
    The accompanying video to follow along: https://youtu.be/JiN8p9MFbTk

--- sdlamp.c
+++ sdlamp.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 #include "SDL.h"
 
+typedef struct
+{
+    SDL_Texture *texture;  // YOU DO NOT OWN THIS POINTER, DON'T FREE IT.
+    SDL_Rect srcrect_unpressed;
+    SDL_Rect srcrect_pressed;
+    SDL_Rect dstrect;
+    SDL_bool pressed;
+} WinAmpSkinButton;
+
+typedef enum
+{
+    WASBTN_PREV=0,
+    WASBTN_PLAY,
+    WASBTN_PAUSE,
+    WASBTN_STOP,
+    WASBTN_NEXT,
+    WASBTN_EJECT,
+    WASBTN_TOTAL
+} WinAmpSkinButtonId;
+
+typedef struct
+{
+    SDL_Texture *tex_main;
+    SDL_Texture *tex_cbuttons;
+    WinAmpSkinButton buttons[WASBTN_TOTAL];
+} WinAmpSkin;
+
 static SDL_AudioDeviceID audio_device = 0;
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
@@ -17,6 +44,7 @@ static void panic_and_abort(const char *title, const char *text)
     exit(1);
 }
 
+static WinAmpSkin skin;
 static float volume_slider_value = 1.0f;
 static float balance_slider_value = 0.5f;
 
@@ -77,7 +105,7 @@ failed:
     return SDL_FALSE;
 }
 
-SDL_Texture *load_texture(const char *fname)
+static SDL_Texture *load_texture(const char *fname)
 {
     SDL_Surface *surface = SDL_LoadBMP(fname);
     if (!surface) {
@@ -90,7 +118,46 @@ SDL_Texture *load_texture(const char *fname)
 }
 
 
-int main(int argc, char **argv)
+static SDL_INLINE void init_skin_button(WinAmpSkinButton *btn, SDL_Texture *tex,
+                                        const int w, const int h,
+                                        const int dx, const int dy,
+                                        const int sxu, const int syu,
+                                        const int sxp, const int syp)
+{
+    btn->texture = tex;
+    btn->srcrect_unpressed.x = sxu;
+    btn->srcrect_unpressed.y = syu;
+    btn->srcrect_unpressed.w = w;
+    btn->srcrect_unpressed.h = h;
+    btn->srcrect_pressed.x = sxp;
+    btn->srcrect_pressed.y = syp;
+    btn->srcrect_pressed.w = w;
+    btn->srcrect_pressed.h = h;
+    btn->dstrect.x = dx;
+    btn->dstrect.y = dy;
+    btn->dstrect.w = w;
+    btn->dstrect.h = h;
+    btn->pressed = SDL_FALSE;
+}
+
+static SDL_bool load_skin(WinAmpSkin *skin, const char *fname)  // !!! FIXME: use this variable
+{
+    SDL_zerop(skin);
+
+    skin->tex_main = load_texture("hifi/Main.bmp");  // !!! FIXME: hardcoded
+    skin->tex_cbuttons = load_texture("hifi/Cbuttons.bmp"); // !!! FIXME: hardcoded
+
+    init_skin_button(&skin->buttons[WASBTN_PREV], skin->tex_cbuttons, 23, 18, 16, 88, 0, 0, 0, 18);
+    init_skin_button(&skin->buttons[WASBTN_PLAY], skin->tex_cbuttons, 23, 18, 39, 88, 23, 0, 23, 18);
+    init_skin_button(&skin->buttons[WASBTN_PAUSE], skin->tex_cbuttons, 23, 18, 62, 88, 46, 0, 46, 18);
+    init_skin_button(&skin->buttons[WASBTN_STOP], skin->tex_cbuttons, 23, 18, 85, 88, 69, 0, 69, 18);
+    init_skin_button(&skin->buttons[WASBTN_NEXT], skin->tex_cbuttons, 22, 18, 108, 88, 92, 0, 92, 18);
+    init_skin_button(&skin->buttons[WASBTN_EJECT], skin->tex_cbuttons, 22, 16, 136, 89, 114, 0, 114, 16);
+
+    return SDL_TRUE;
+}
+
+static void init_everything(int argc, char **argv)
 {
     SDL_AudioSpec desired;
 
@@ -108,14 +175,8 @@ int main(int argc, char **argv)
         panic_and_abort("SDL_CreateRenderer failed", SDL_GetError());
     }
 
-    SDL_Texture *skin_main = load_texture("hifi/Main.bmp");
-    if (!skin_main) {
-        panic_and_abort("Failed to load skin main.bmp", SDL_GetError());
-    }
-
-    SDL_Texture *skin_cbuttons = load_texture("hifi/Cbuttons.bmp");
-    if (!skin_main) {
-        panic_and_abort("Failed to load skin cbuttons.bmp", SDL_GetError());
+    if (!load_skin(&skin, "")) {  // !!! FIXME: load a real thing, not an empty string
+        panic_and_abort("Failed to load initial skin", SDL_GetError());
     }
 
     SDL_zero(desired);
@@ -133,16 +194,173 @@ int main(int argc, char **argv)
     SDL_EventState(SDL_DROPFILE, SDL_ENABLE);  // tell SDL we want this event that is disabled by default.
 
     open_new_audio_file("music.wav");
+}
 
-    SDL_bool paused = SDL_TRUE;
-    const SDL_Rect rewind_rect = { 100, 100, 100, 100 };
-    const SDL_Rect stop_rect = { 275, 100, 100, 100 };
-    const SDL_Rect pause_rect = { 62, 88, 23, 18 };
-    const SDL_Rect volume_rect = { (640-500) / 2, 400, 500, 20 };
-    const SDL_Rect balance_rect = { (640-500) / 2, 300, 500, 20 };
-    const SDL_Rect cbuttons_pause_rect = { 46, 0, 23, 18 };
-    const SDL_Rect cbuttons_pause_rect_pressed = { 46, 18, 23, 18 };
+static void draw_button(SDL_Renderer *renderer, WinAmpSkinButton *btn)
+{
+    const SDL_bool pressed = btn->pressed;
+    if (btn->texture == NULL) {
+        if (pressed) {
+            SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+        } else {
+            SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        }
+        SDL_RenderFillRect(renderer, &btn->dstrect);
+    } else {
+        SDL_RenderCopy(renderer, btn->texture, pressed ? &btn->srcrect_pressed : &btn->srcrect_unpressed, &btn->dstrect);
+    }
+}
 
+static void draw_frame(SDL_Renderer *renderer, WinAmpSkin *skin)
+{
+    int i;
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    SDL_RenderCopy(renderer, skin->tex_main, NULL, NULL);
+
+    for (i = 0; i < SDL_arraysize(skin->buttons); i++) {
+        draw_button(renderer, &skin->buttons[i]);
+    }
+
+    SDL_RenderPresent(renderer);
+}
+
+static void feed_more_audio(void)
+{
+    if (SDL_GetQueuedAudioSize(audio_device) < 8192) {
+        const int bytes_remaining = SDL_AudioStreamAvailable(stream);
+        if (bytes_remaining > 0) {
+            const int new_bytes = SDL_min(bytes_remaining, 32 * 1024);
+            static Uint8 converted_buffer[32 * 1024];
+            const int num_converted_bytes = SDL_AudioStreamGet(stream, converted_buffer, new_bytes);
+            if (num_converted_bytes > 0) {
+                const int num_samples = (num_converted_bytes / sizeof (float));
+                float *samples = (float *) converted_buffer;
+
+                SDL_assert((num_samples % 2) == 0);  // this should always be stereo data (at least for now).
+
+                // change the volume of the audio we're playing.
+                if (volume_slider_value != 1.0f) {
+                    for (int i = 0; i < num_samples; i++) {
+                        samples[i] *= volume_slider_value;
+                    }
+                }
+
+                // first sample is left, second is right.
+                // change the balance of the audio we're playing.
+                if (balance_slider_value > 0.5f) {
+                    for (int i = 0; i < num_samples; i += 2) {
+                        samples[i] *= 1.0f - balance_slider_value;
+                    }
+                } else if (balance_slider_value < 0.5f) {
+                    for (int i = 0; i < num_samples; i += 2) {
+                        samples[i+1] *= balance_slider_value;
+                    }
+                }
+
+                SDL_QueueAudio(audio_device, converted_buffer, num_converted_bytes);
+            }
+        }
+    }
+}
+
+static void deinit_everything(void)
+{
+    // !!! FIXME: free_skin()
+    SDL_FreeWAV(wavbuf);
+    SDL_CloseAudioDevice(audio_device);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
+static SDL_bool paused = SDL_TRUE;  // !!! FIXME: move this later.
+
+static SDL_bool handle_events(WinAmpSkin *skin)
+{
+    SDL_Event e;
+    while (SDL_PollEvent(&e)) {
+        switch (e.type) {
+            case SDL_QUIT:
+                return SDL_FALSE;  // don't keep going.
+
+            case SDL_MOUSEBUTTONUP:
+            case SDL_MOUSEBUTTONDOWN: {
+                const SDL_bool pressed = (e.button.state == SDL_PRESSED) ? SDL_TRUE : SDL_FALSE;
+                const SDL_Point pt = { e.button.x, e.button.y };
+                int i;
+
+                for (i = 0; i < SDL_arraysize(skin->buttons); i++) {
+                    WinAmpSkinButton *btn = &skin->buttons[i];
+                    btn->pressed = (pressed && SDL_PointInRect(&pt, &btn->dstrect)) ? SDL_TRUE : SDL_FALSE;
+                    if (btn->pressed) {
+                        switch ((WinAmpSkinButtonId) i) {
+                            case WASBTN_PREV:
+                                SDL_ClearQueuedAudio(audio_device);
+                                SDL_AudioStreamClear(stream);
+                                if (SDL_AudioStreamPut(stream, wavbuf, wavlen) == -1) {
+                                    SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Audio stream put failed", SDL_GetError(), window);
+                                    stop_audio();
+                                } else if (SDL_AudioStreamFlush(stream) == -1) {
+                                    SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Audio stream flush failed", SDL_GetError(), window);
+                                    stop_audio();
+                                }
+                                break;
+
+                            case WASBTN_PAUSE:
+                                paused = paused ? SDL_FALSE : SDL_TRUE;
+                                SDL_PauseAudioDevice(audio_device, paused);
+                                break;
+
+                            case WASBTN_STOP:
+                                stop_audio();
+                                break;
+                        }
+                    }
+                }
+                break;
+            }
+
+            #if 0
+            case SDL_MOUSEMOTION: {
+                const SDL_Point pt = { e.motion.x, e.motion.y };
+                if (SDL_PointInRect(&pt, &volume_rect) && (e.motion.state & SDL_BUTTON_LMASK)) {  // mouse is pressed inside the "volume" "slider"?
+                    const float fx = (float) (pt.x - volume_rect.x);
+                    volume_slider_value = (fx / ((float) volume_rect.w));  // a value between 0.0f and 1.0f
+                    //printf("SLIDING! At %dx%d (%d percent)\n", pt.x, pt.y, (int) SDL_round(volume_slider_value * 100.0f));
+                    volume_knob.x = pt.x - (volume_knob.w / 2);
+                    volume_knob.x = SDL_max(volume_knob.x, volume_rect.x);
+                    volume_knob.x = SDL_min(volume_knob.x, (volume_rect.x + volume_rect.w) - volume_knob.w);
+                } else if (SDL_PointInRect(&pt, &balance_rect) && (e.motion.state & SDL_BUTTON_LMASK)) {  // mouse is pressed inside the "balance" "slider"?
+                    const float fx = (float) (pt.x - balance_rect.x);
+                    balance_slider_value = (fx / ((float) balance_rect.w));  // a value between 0.0f and 1.0f
+                    //printf("SLIDING! At %dx%d (%d percent)\n", pt.x, pt.y, (int) SDL_round(balance_slider_value * 100.0f));
+                    balance_knob.x = pt.x - (balance_knob.w / 2);
+                    balance_knob.x = SDL_max(balance_knob.x, balance_rect.x);
+                    balance_knob.x = SDL_min(balance_knob.x, (balance_rect.x + balance_rect.w) - balance_knob.w);
+                }
+                break;
+            }
+            #endif
+
+            case SDL_DROPFILE: {
+                open_new_audio_file(e.drop.file);
+                SDL_free(e.drop.file);
+                break;
+            }
+        }
+    }
+
+    return SDL_TRUE;  // keep going.
+}
+
+int main(int argc, char **argv)
+{
+    init_everything(argc, argv);  // will panic_and_abort on issues.
+
+    #if 0   // !!! FIXME: do something with this.
     SDL_Rect volume_knob;
     volume_knob.y = volume_rect.y;
     volume_knob.h = volume_rect.h;
@@ -154,136 +372,17 @@ int main(int argc, char **argv)
     balance_knob.h = balance_rect.h;
     balance_knob.w = 20;
     balance_knob.x = (balance_rect.x + (balance_rect.w / 2)) - balance_knob.w;
+    #endif
 
-    SDL_bool cbutton_pause_pressed = SDL_FALSE;
-
-    SDL_bool keep_going = SDL_TRUE;
-    while (keep_going) {
-        SDL_Event e;
-        while (SDL_PollEvent(&e)) {
-            switch (e.type) {
-                case SDL_QUIT:
-                    keep_going = SDL_FALSE;
-                    break;
-
-                case SDL_MOUSEBUTTONUP:
-                case SDL_MOUSEBUTTONDOWN: {
-                    const SDL_bool pressed = (e.button.state == SDL_PRESSED) ? SDL_TRUE : SDL_FALSE;
-                    const SDL_Point pt = { e.button.x, e.button.y };
-                    if (SDL_PointInRect(&pt, &rewind_rect)) {  // pressed the "rewind" button
-                        SDL_ClearQueuedAudio(audio_device);
-                        SDL_AudioStreamClear(stream);
-                        if (SDL_AudioStreamPut(stream, wavbuf, wavlen) == -1) {
-                            SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Audio stream put failed", SDL_GetError(), window);
-                            stop_audio();
-                        } else if (SDL_AudioStreamFlush(stream) == -1) {
-                            SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Audio stream flush failed", SDL_GetError(), window);
-                            stop_audio();
-                        }
-                    } else if (SDL_PointInRect(&pt, &pause_rect)) {  // pressed the "pause" button
-                        cbutton_pause_pressed = pressed;
-                        if (cbutton_pause_pressed) {
-                            paused = paused ? SDL_FALSE : SDL_TRUE;
-                            SDL_PauseAudioDevice(audio_device, paused);
-                        }
-                    } else if (SDL_PointInRect(&pt, &stop_rect)) {  // pressed the "stop" button
-                        stop_audio();
-                    }
-                    break;
-                }
-
-                case SDL_MOUSEMOTION: {
-                    const SDL_Point pt = { e.motion.x, e.motion.y };
-                    if (SDL_PointInRect(&pt, &volume_rect) && (e.motion.state & SDL_BUTTON_LMASK)) {  // mouse is pressed inside the "volume" "slider"?
-                        const float fx = (float) (pt.x - volume_rect.x);
-                        volume_slider_value = (fx / ((float) volume_rect.w));  // a value between 0.0f and 1.0f
-                        //printf("SLIDING! At %dx%d (%d percent)\n", pt.x, pt.y, (int) SDL_round(volume_slider_value * 100.0f));
-                        volume_knob.x = pt.x - (volume_knob.w / 2);
-                        volume_knob.x = SDL_max(volume_knob.x, volume_rect.x);
-                        volume_knob.x = SDL_min(volume_knob.x, (volume_rect.x + volume_rect.w) - volume_knob.w);
-                    } else if (SDL_PointInRect(&pt, &balance_rect) && (e.motion.state & SDL_BUTTON_LMASK)) {  // mouse is pressed inside the "balance" "slider"?
-                        const float fx = (float) (pt.x - balance_rect.x);
-                        balance_slider_value = (fx / ((float) balance_rect.w));  // a value between 0.0f and 1.0f
-                        //printf("SLIDING! At %dx%d (%d percent)\n", pt.x, pt.y, (int) SDL_round(balance_slider_value * 100.0f));
-                        balance_knob.x = pt.x - (balance_knob.w / 2);
-                        balance_knob.x = SDL_max(balance_knob.x, balance_rect.x);
-                        balance_knob.x = SDL_min(balance_knob.x, (balance_rect.x + balance_rect.w) - balance_knob.w);
-                    }
-                    break;
-                }
-
-                case SDL_DROPFILE: {
-                    open_new_audio_file(e.drop.file);
-                    SDL_free(e.drop.file);
-                    break;
-                }
-            }
-        }
-
-        if (SDL_GetQueuedAudioSize(audio_device) < 8192) {
-            const int bytes_remaining = SDL_AudioStreamAvailable(stream);
-            if (bytes_remaining > 0) {
-                const int new_bytes = SDL_min(bytes_remaining, 32 * 1024);
-                static Uint8 converted_buffer[32 * 1024];
-                const int num_converted_bytes = SDL_AudioStreamGet(stream, converted_buffer, new_bytes);
-                if (num_converted_bytes > 0) {
-                    const int num_samples = (num_converted_bytes / sizeof (float));
-                    float *samples = (float *) converted_buffer;
-
-                    SDL_assert((num_samples % 2) == 0);  // this should always be stereo data (at least for now).
-
-                    // change the volume of the audio we're playing.
-                    if (volume_slider_value != 1.0f) {
-                        for (int i = 0; i < num_samples; i++) {
-                            samples[i] *= volume_slider_value;
-                        }
-                    }
-
-                    // first sample is left, second is right.
-                    // change the balance of the audio we're playing.
-                    if (balance_slider_value > 0.5f) {
-                        for (int i = 0; i < num_samples; i += 2) {
-                            samples[i] *= 1.0f - balance_slider_value;
-                        }
-                    } else if (balance_slider_value < 0.5f) {
-                        for (int i = 0; i < num_samples; i += 2) {
-                            samples[i+1] *= balance_slider_value;
-                        }
-                    }
-
-                    SDL_QueueAudio(audio_device, converted_buffer, num_converted_bytes);
-                }
-            }
-        }
-
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-        SDL_RenderClear(renderer);
-
-        SDL_RenderCopy(renderer, skin_main, NULL, NULL);
-        SDL_RenderCopy(renderer, skin_cbuttons, cbutton_pause_pressed ? &cbuttons_pause_rect_pressed : &cbuttons_pause_rect, &pause_rect);
-
-        #if 0
-        SDL_RenderFillRect(renderer, &rewind_rect);
-        SDL_RenderFillRect(renderer, &stop_rect);
-        SDL_RenderFillRect(renderer, &pause_rect);
-        SDL_RenderFillRect(renderer, &volume_rect);
-        SDL_RenderFillRect(renderer, &balance_rect);
-
-        SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
-        SDL_RenderFillRect(renderer, &volume_knob);
-        SDL_RenderFillRect(renderer, &balance_knob);
-        #endif
-
-        SDL_RenderPresent(renderer);
+    while (handle_events(&skin)) {
+        feed_more_audio();
+        draw_frame(renderer, &skin);
     }
 
-    SDL_FreeWAV(wavbuf);
-    SDL_CloseAudioDevice(audio_device);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-
-    SDL_Quit();
+    deinit_everything();
 
     return 0;
 }
+
+// end of sdlamp.c ...
 
